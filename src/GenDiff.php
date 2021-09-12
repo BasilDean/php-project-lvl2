9e3c6830d0526d697b8075e0ee1e22d3e9088f1c
Василий <vonavud@gmail.com>
2021-09-12 11:02:04 +0300

    first step

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -4,15 +4,19 @@ namespace GenDiff\GenDiff;
 
 const DOC = <<<DOC
 
+gendiff -h
+
 Generate diff
 
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
   
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
 DOC;
 
 function run()
